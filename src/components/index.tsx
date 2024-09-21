5ff55a44993b22e0875eeb5d7fea9bda7db5b838
Christian Francis <cfrancis1989@gmail.com>
2024-09-21 19:14:24 -0400

    add intro

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
 export { default as BackgroundAnimation } from "./background-animation";
+export { default as BaldManAnimation } from "./intro-animation";
 export { default as Header } from "./header";
+export { default as Intro } from "./intro";
