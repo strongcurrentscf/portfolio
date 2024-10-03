4354562cbfcd6521ca4faeda0f3605c9a8b0eeb3
Christian Francis <cfrancis1989@gmail.com>
2024-10-03 19:29:12 -0400

    update sendEmail imports

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,7 +1,7 @@
 import { createElement } from "react";
 import { Resend } from "resend";
 import { renderToStaticMarkup } from "react-dom/server";
-import { ContactFormEmail } from "email";
+import { ContactFormEmail } from "../../src/email";
 import dotenv from "dotenv";
 
 dotenv.config(); // Make sure to load environment variables
